52a368d1ff457d42f0546051445d1a6c0692ced9
hesouu <hikari251025@gmail.com>
2026-08-20 10:51:58 +0900

    feat: 파일 내용 추가

--- infra/iam.tf
+++ infra/iam.tf
@@ -23,3 +23,41 @@ resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role       = aws_iam_role.ecs_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# [브론즈 추가]
+# Firehose용 IAM Role/Policy 추가
+# aws_iam_policy_document -> aws_iam_role -> aws_iam_role_policy_attachment(필요시 추가)
+
+# firehose에서 iam role을 사용하도록 신뢰 정책 조회
+data "aws_iam_policy_document" "firehose_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+  }  
+}
+resource "aws_iam_role" "firehose" {
+  # role의 이름 -> 고유
+  name               = "${var.project_name}-firehose-role"
+  # role에 적용되는 정책 -> 어떤 권한을 가지는가?
+  assume_role_policy = data.aws_iam_policy_document.firehose_assume.json
+}
+
+# Firehose가 입력으로 kinesis에서 읽어오는, 출력으로 s3에 저장, 권한 
+data "aws_iam_policy_document" "firehose_s3" {
+  # kinesis 읽기 권한 관련  
+
+  # s3 저장 권한 관련
+}
+
+
+# firehose_s3를 통해서 조회한 권한을 aws_iam_role.firehose 에 부여
+resource "aws_iam_role_policy" "firehose" {
+  name = "${var.project_name}-firehose-s3-policy"
+  role = aws_iam_role.firehose.id
+  policy = data.aws_iam_policy_document.firehose_s3.json
+}
